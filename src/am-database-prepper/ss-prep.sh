62988274585336bc61aab8bd5dd5293b61e81d19
Simon Bruce-Cassidy <simon.brucecassidy@piql.com>
2021-03-22 17:12:39 +0100

    Deleted the db prep pod

--- src/am-database-prepper/ss-prep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mysql -hmysql -upiqlconnect@archivematica-mysql -pServersAreKewl! -e "\
-	DROP DATABASE IF EXISTS SS; \
-	CREATE DATABASE SS; \
-    CREATE USER 'archivematica'@'%' IDENTIFIED BY 'demo'; \
-	GRANT ALL ON SS.* TO 'archivematica'@'%';"
